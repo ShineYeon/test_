db9eed6e7ae73b09974267fb9d1bf5f6b008a815
최희연 <chlgmldus2@gmail.com>
2021-05-03 13:00:39 +0900

    Initial Commit

---
+++ testing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testing
+//
+//  Created by 최희연 on 2021/05/03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
